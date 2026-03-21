3cab7a0d50816755b047f456ddda146b7ddfe2f0
gwillembeens <g.willem.beens@gmail.com>
2026-03-22 00:18:44 +0100

    feat(10-03-02): register historySlice in Redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import validatorReducer from './slices/validatorSlice.js'
 import authReducer from './slices/authSlice.js'
+import historyReducer from './slices/historySlice.js'
 
 export const store = configureStore({
   reducer: {
     validator: validatorReducer,
     auth: authReducer,
+    history: historyReducer,
   },
 })
